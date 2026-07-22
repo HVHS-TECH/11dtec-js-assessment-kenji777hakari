758788ae4599c1e14f740c87e98e7cdf5cfa2a58
Fat Naoya <24345jh@hvhs.school.nz>
2026-07-22 00:03:00 +0000

    Jul 22, 2026, 12:03 AM

--- script.js
+++ script.js
@@ -14,11 +14,11 @@ let food = ["Food 1","Food 2, Food 3"]
 /**********************************************************
  MAIN CODE
  ********************************************/
- userName = prompt("Welcome to my Triple T's Pizza Palza! please enter your name?");
+ userName = prompt("Welcome to Triple T's Pizza Palza! please enter your name?");
 alert("your name is " + userName);
 
 age = prompt("Before continuing to Triple T's Pizza Palza please type in your age");
  locations = prompt("Before continuing further please type in your location");
 alert("Hi " + userName +  ", you are "+ age +". You are currently at "+ locations +". welcome to Triple T's Pizza Plaza.");
 alert("What would you like to order?")
-food = prompt ("Menu: Brr Brr PataBurrito, Sahur Pizza 2, Tralalaro TralaTacos 3,   ")
+food = prompt ("Menu: Brr Brr PataBurrito, Sahur Pizza 2, Tralalaro TralaTacos 3,  ")
